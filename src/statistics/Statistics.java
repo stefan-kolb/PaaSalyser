f2510dc1d002f8df68ecafef510b9b8fa68b5557
Robert <robert.mueller@stud.uni-bamberg.de>
2017-06-03 19:53:02 +0200

    Further work in statistics

--- src/statistics/Statistics.java
+++ src/statistics/Statistics.java
@@ -8,8 +8,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
-import org.apache.commons.math3.stat.descriptive.moment.Mean;
-import org.apache.commons.math3.stat.descriptive.moment.StandardDeviation;
 
 import models.PaasProfile;
 import models.PricingModel;
@@ -71,34 +69,26 @@ public class Statistics {
 		results.put("latest", Double.MAX_VALUE);
 		results.put("oldest", Double.MIN_VALUE);
 		results.put("mean", 0.0);
-		results.put("stdev", 0.0);
-		
-		double[] statusSince = new double[profiles.size()];
-		int i = 0;
-		
+
 		profiles.forEach(profile -> {
 			double statusSinceAge = 0;
 			if (!profile.getStatusSince().equalsIgnoreCase("null")) {
 				try {
 					statusSinceAge = ChronoUnit.DAYS.between(LocalDate.parse(profile.getStatusSince().substring(0, 10)),
 							LocalDate.now());
-					statusSince[i] = statusSinceAge;
 					if (statusSinceAge != 0 && statusSinceAge < results.get("latest")) {
 						results.replace("latest", results.get("latest"), statusSinceAge);
 					}
 					if (statusSinceAge != 0 && statusSinceAge > results.get("oldest")) {
 						results.replace("oldest", results.get("oldest"), statusSinceAge);
 					}
+					results.replace("mean", results.get("mean"), results.get("mean") + statusSinceAge);
 				} catch (DateTimeParseException e) {
-					System.out.println("x");
 					System.out.println(e.getMessage());
 				}
 			}
 		});
-//		results.replace("mean", results.get("mean"), results.get("mean") / profiles.size());
-		// TODO Fix Calculations
-		results.replace("mean", results.get("mean"), new Mean().evaluate(statusSince));
-		results.replace("stdev", results.get("stdev"), new StandardDeviation().evaluate(statusSince));
+		results.replace("mean", results.get("mean"), results.get("mean") / profiles.size());
 		return results;
 	}
 
@@ -152,6 +142,7 @@ public class Statistics {
 		results.put("monthly", (long) 0);
 		results.put("anually", (long) 0);
 		results.put("period-empty", (long) 0);
+
 		profiles.forEach(profile -> {
 			if (profile.getPricings().size() == 0) {
 				results.replace("modelcounter0", results.get("modelcounter0"), results.get("modelcounter0") + 1);
@@ -189,6 +180,94 @@ public class Statistics {
 			});
 		});
 		return results;
+	}
+
+	public static Map<String, Long> evalScaling(List<PaasProfile> profiles) {
+		Map<String, Long> results = new HashMap<String, Long>();
+		results.put("vertical", (long) 0);
+		results.put("horizontal", (long) 0);
+		results.put("auto", (long) 0);
+
+		profiles.forEach(profile -> {
+			if (profile.getScaling().getVertical()) {
+				results.replace("vertical", results.get("vertical"), results.get("vertical") + 1);
+			}
+			if (profile.getScaling().getHorizontal()) {
+				results.replace("horizontal", results.get("horizontal"), results.get("horizontal") + 1);
+			}
+			if (profile.getScaling().getAuto()) {
+				results.replace("auto", results.get("auto"), results.get("auto") + 1);
+			}
+		});
+		return results;
+	}
+
+	public static Map<String, Long> evalRuntimes(List<PaasProfile> profiles) {
+		Map<String, Long> results = new HashMap<String, Long>();
+		results.put("apex", (long) 0);
+		results.put("clojure", (long) 0);
+		results.put("cobol", (long) 0);
+		results.put("dotnet", (long) 0);
+		results.put("erlang", (long) 0);
+		results.put("go", (long) 0);
+		results.put("groovy", (long) 0);
+		results.put("haskell", (long) 0);
+		results.put("java", (long) 0);
+		results.put("lua", (long) 0);
+		results.put("node", (long) 0);
+		results.put("perl", (long) 0);
+		results.put("php", (long) 0);
+		results.put("python", (long) 0);
+		results.put("ruby", (long) 0);
+		results.put("scala", (long) 0);
+
+		profiles.forEach(profile -> {
+			profile.getRuntimes().forEach(runtime -> {
+				if (runtime.getLanguage().equalsIgnoreCase("apex")) {
+					results.replace("apex", results.get("apex"), results.get("apex") + 1);
+					runtime.getVersions().forEach(version -> {
+						if (!results.containsKey("apex-version" + version)) {
+							results.replace("apex-version" + version, results.get("apex-version" + version),
+									results.get("apex-version" + version) + 1);
+						} else {
+							results.put("apex-version_" + version, (long) 1);
+						}
+					});
+				} else if (runtime.getLanguage().equalsIgnoreCase("clojure")) {
+					results.replace("clojure", results.get("clojure"), results.get("clojure") + 1);
+				} else if (runtime.getLanguage().equalsIgnoreCase("cobol")) {
+					results.replace("cobol", results.get("cobol"), results.get("cobol") + 1);
+				} else if (runtime.getLanguage().equalsIgnoreCase("dotnet")) {
+					results.replace("dotnet", results.get("dotnet"), results.get("dotnet") + 1);
+				} else if (runtime.getLanguage().equalsIgnoreCase("erlang")) {
+					results.replace("erlang", results.get("erlang"), results.get("erlang") + 1);
+				} else if (runtime.getLanguage().equalsIgnoreCase("go")) {
+					results.replace("go", results.get("go"), results.get("go") + 1);
+				} else if (runtime.getLanguage().equalsIgnoreCase("groovy")) {
+					results.replace("groovy", results.get("groovy"), results.get("groovy") + 1);
+				} else if (runtime.getLanguage().equalsIgnoreCase("haskell")) {
+					results.replace("haskell", results.get("haskell"), results.get("haskell") + 1);
+				} else if (runtime.getLanguage().equalsIgnoreCase("java")) {
+					results.replace("java", results.get("java"), results.get("java") + 1);
+				} else if (runtime.getLanguage().equalsIgnoreCase("lua")) {
+					results.replace("lua", results.get("lua"), results.get("lua") + 1);
+				} else if (runtime.getLanguage().equalsIgnoreCase("node")) {
+					results.replace("node", results.get("node"), results.get("node") + 1);
+				} else if (runtime.getLanguage().equalsIgnoreCase("perl")) {
+					results.replace("perl", results.get("perl"), results.get("perl") + 1);
+				} else if (runtime.getLanguage().equalsIgnoreCase("php")) {
+					results.replace("php", results.get("php"), results.get("php") + 1);
+				} else if (runtime.getLanguage().equalsIgnoreCase("python")) {
+					results.replace("python", results.get("python"), results.get("python") + 1);
+				} else if (runtime.getLanguage().equalsIgnoreCase("ruby")) {
+					results.replace("ruby", results.get("ruby"), results.get("ruby") + 1);
+				} else if (runtime.getLanguage().equalsIgnoreCase("scala")) {
+					results.replace("scala", results.get("scala"), results.get("scala") + 1);
+				}
+			});
+		});
+
+		return null;
 	}
 
 }
